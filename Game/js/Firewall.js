c4ddbc05882b715216e2bbbf9f6cb759653b121a
mika <mail@deadcells.org>
2015-12-03 13:55:06 +0100

    Added gap generation
    Uses the co-ordinates of the last gap created and makes the next gap be within +- 300 pixels of the last one (restricted by boundaries)

--- Game/js/Firewall.js
+++ Game/js/Firewall.js
@@ -5,23 +5,24 @@ MainframeGame.Firewall = function (game) {
 	this.elementLayer = null;
 	this.timerLayer = null;
 	this.monitorLayer = null;
-	
+
 	// Timer
-	
+
 	this.timerBlock = null;
 	this.timerTime = null;
 	this.timerStartTime = null;
 	this.timeGoal = Phaser.Timer.SECOND * 20;
-	
+
 	// Firewall stuff
 	this.gapSize = 150;
 	this.rows = [];
-	
+	this.lastGapX = null;
+
 	this.player = null;
 	this.cursors = null;
-	
+
 	this.music = null;
-	
+
 	this.ready = false;
 
 };
@@ -33,23 +34,24 @@ MainframeGame.Firewall.prototype = {
 		this.timerLayer = this.game.add.group();
 		this.monitorLayer = this.game.add.group();
 		this.monitorLayer.add(this.game.add.sprite(0,0,'atlas','General/monitor.png'));
-		
+
 		this.player = this.game.add.sprite(0, 425, 'atlas', 'Subroutines/General/player_skull.png');
 		this.player.scale.setTo(0.5, 0.5);
 		centreSprite(this.player, this.game.width);
 
 		this.cursors = this.game.input.keyboard.createCursorKeys();
-		
-		this.createRow(0, 300);		
-		this.createRow(150, 300);
-		this.createRow(300, 300);
-		
+
+		this.createRow(0, 370);
+		this.lastGapX = 370;
+		this.generateRow(150);
+		this.generateRow(300);
+
 		this.initTimer();
-		
+
 	},
-	
-	update: function () {		
-	
+
+	update: function () {
+
 		if (this.ready)
 		{
 			// Update
@@ -57,14 +59,14 @@ MainframeGame.Firewall.prototype = {
 				this.timerTime = this.game.time.now;
 				this.timerStartTime = this.timerTime;
 			}
-			
+
 			if(this.game.time.now - this.timerTime >= Phaser.Timer.SECOND)
 			{
 				this.timerTime = this.game.time.now;
 				this.incTimer();
 			}
-			
-			// Player movement 
+
+			// Player movement
 			if (this.cursors.left.isDown)
 			{
 				if (this.player.x >= 35) {
@@ -77,85 +79,103 @@ MainframeGame.Firewall.prototype = {
 					this.player.x += 5;
 				}
 			}
-			
+
 			// Moves the bars. Although maybe tweening would be better?
 			// Also destroys any bar and adds a new one if off screen
 			for (var i = 0; i < this.rows.length; i++) {
 				for (var x = 0; x < this.rows[i].length; x++) {
-					this.rows[i][x].y += 1;					
+					this.rows[i][x].y += 3;
 				}
-				
+
 				if (this.rows[i][0].y > this.game.height - 50) {
 					for (var x = 0; x < this.rows[i].length; x++) {
 						this.rows[i][x].destroy();
 					}
-					
+
 					this.rows.splice(i, 1);
-					
-					this.createRow(-50, 300);
-					
+
+					this.generateRow(-50);
+
 					// Un-increment the i pointer
 					i -= 1;
 				}
-				
-				
+
+
 			}
 
 		}
     },
 
-    victory: function () {				
+    victory: function () {
 		var victorySign = this.game.add.sprite(0, 200, 'subroutine_complete');
 		this.timerLayer.add(victorySign);
 		centreSprite(victorySign, this.game.width);
-		victorySign.animations.add('anim');		
+		victorySign.animations.add('anim');
 		victorySign.animations.play('anim', 16, false);
 	},
-	
+
 	failure: function () {
-		
+
 	},
-	
+
+	generateRow: function(y) {
+		var left = Math.floor(Math.random()*300);
+		var right = Math.floor(Math.random()*300);
+
+		this.lastGapX += left - right;
+
+		// Bounds checking
+		if (this.lastGapX < 45) {
+			this.lastGapX = 45;
+		}
+
+		if (this.lastGapX + this.gapSize > 915) {
+			this.lastGapX = 915 - this.gapSize;
+		}
+
+		this.createRow(y, this.lastGapX);
+	},
+
 	createRow: function (y, gapX) {
 		var newRow = [];
-		
+
 		var gapLeft = this.game.add.sprite(gapX, y, 'atlas', 'Subroutines/Firewall/firewall_end_left.png');
 		this.elementLayer.add(gapLeft);
 		newRow.push(gapLeft);
-		
-		
+
+
 		var gapRight = this.game.add.sprite(gapX + this.gapSize, y, 'atlas', 'Subroutines/Firewall/firewall_end_right.png');
 		this.elementLayer.add(gapRight);
 		newRow.push(gapRight);
-		
+
 		var newX = gapLeft.x;
 
 		while (newX > 0) {
 			newX -= 61;
 			newRow.push(this.addBlock(newX, y));
 		}
-		
+
 		newX = gapRight.x + 12;
 		while (newX < this.game.width) {
 			newX += 61;
 			newRow.push(this.addBlock(newX, y));
 		}
-		
+
 		this.rows.push(newRow);
-		
+
 	},
-	
+
 	addBlock: function (x, y) {
 		var block = this.game.add.sprite(x, y, 'atlas', 'Subroutines/Firewall/firewall_block.png');
 		this.elementLayer.add(block);
 		return block;
 	},
-	
+
 	initTimer: function (context) {
 		var timerBar = this.game.add.sprite(0, 22, 'icebreak_in_progress');
 		this.timerLayer.add(timerBar);
 		centreSprite(timerBar, this.game.width);
-		timerBar.animations.add('anim');		
+		timerBar.animations.add('anim');
 		timerBar.animations.play('anim', 16, false);
 		timerBar.events.onAnimationComplete.add(function() {
 				this.ready = true;
@@ -165,14 +185,14 @@ MainframeGame.Firewall.prototype = {
 		this.timerBlock = this.game.add.sprite(10,54,'atlas', 'Subroutines/General/trace_bar_full.png');
 		centreSprite(this.timerBlock, this.game.width);
 		this.timerLayer.add(this.timerBlock);
-		
-		this.timerBlock.width = 0;	
+
+		this.timerBlock.width = 0;
 	},
-	
+
 	incTimer: function () {
 		var barWidth = 859;
 		var percentage = (this.game.time.now - this.timerStartTime) / this.timeGoal;
-		this.timerBlock.width = percentage*barWidth;	
+		this.timerBlock.width = percentage*barWidth;
 
 		if (percentage >= 1)
 		{
