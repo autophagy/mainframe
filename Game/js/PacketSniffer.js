1e298f1bf7029fde2450c954ce41f74fa4e0d831
Mika <mail@deadcells.org>
2015-12-12 22:54:00 +0100

    Added sniffer failure animation

--- Game/js/PacketSniffer.js
+++ Game/js/PacketSniffer.js
@@ -2,6 +2,7 @@ Mainframe.PacketSniffer = function (game) {
 
 	// Standard Layering
 
+	this.streamLayer = null;
 	this.elementLayer = null;
     this.tutorialLayer = null;
 	this.timerLayer = null;
@@ -13,16 +14,22 @@ Mainframe.PacketSniffer = function (game) {
 	this.timerTime = null;
 	this.timerStartTime = null;
 	this.timeLimit = Phaser.Timer.SECOND * 60;
+	
+	this.playerSkull = null;
+	this.packetBar = null;
+	this.sniffer = null;
 
 	this.music = null;
 
 	this.ready = false;
+	this.enabled = false;
 
 };
 
 Mainframe.PacketSniffer.prototype = {
 
 	create: function () {
+		this.streamLayer = this.game.add.group();
 		this.elementLayer = this.game.add.group();
         this.tutorialLayer = this.game.add.group();
 		this.timerLayer = this.game.add.group();
@@ -38,7 +45,9 @@ Mainframe.PacketSniffer.prototype = {
         t += '\n	Capturing non-key packets will lock up the sniffer and require';
         t += '\n	it to be rebooted.';
 
-		Mainframe.setupTutorial(this, t);
+		//Mainframe.setupTutorial(this, t);
+		this.setupGame();
+		this.initGame();
 	},
 
 	update: function () {
@@ -56,11 +65,36 @@ Mainframe.PacketSniffer.prototype = {
     },
 
     setupGame: function () {
+	
+	 this.playerSkull = Mainframe.centreSprite(this.game.add.sprite(0,90,'atlas', 'Subroutines/General/player_skull.png'), this.game.width);
+	 this.elementLayer.add(this.playerSkull);
+	 this.elementLayer.add(Mainframe.centreSprite(this.game.add.sprite(0,170,'atlas', 'Subroutines/Packet_Sniffer/progress_bar.png'), this.game.width));
+	 this.sniffer = Mainframe.centreSprite(this.game.add.sprite(0,210,'atlas', 'Subroutines/Packet_Sniffer/pipe.png'), this.game.width);
+	 this.elementLayer.add(this.sniffer);
 
     },
 
     initGame: function () {
         Mainframe.initTimer(this, true);
-    }
+    },
+	
+	disableSniffer: function () {
+		this.enabled = false;
+		this.sniffer.alpha = 0;
+		this.playerSkull.alpha = 0;
+		
+		var errorSkull = this.game.add.sprite(this.playerSkull.x, this.playerSkull.y, 'skull_error');
+		this.elementLayer.add(errorSkull);
+    	errorSkull.animations.add('anim');
+    	errorSkull.animations.play('anim', 32, false);
+    	errorSkull.events.onAnimationComplete.add(function() {
+			this.game.time.events.add(Phaser.Timer.SECOND * 4, function() {
+				errorSkull.destroy();
+				this.sniffer.alpha = 1;
+				this.playerSkull.alpha = 1;
+				this.enabled = true;
+			}, this);
+		}, this);
+	}
 
 };
