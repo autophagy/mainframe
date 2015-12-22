6a78b590b2e4698959e846f410ac8d28c4ae80c7
mika <mail@deadcells.org>
2015-12-22 17:13:17 +0100

    Added proper layering

--- Game/js/VictoryScreen.js
+++ Game/js/VictoryScreen.js
@@ -1,5 +1,8 @@
 Mainframe.VictoryScreen = function (game) {
 
+	this.elementLayer = null;
+	this.monitorLayer = null;
+
 	this.music = null;
 
 	this.dataValueText = null;
@@ -17,16 +20,18 @@ Mainframe.VictoryScreen = function (game) {
 Mainframe.VictoryScreen.prototype = {
 
 	create: function () {
+		this.elementLayer = this.game.add.group();
+		this.monitorLayer = this.game.add.group();
 
-		Mainframe.centreSprite(this.game.add.sprite(0, 70, 'atlas', 'Victory_Screen/run_successful.png'), this.game.width);
-		Mainframe.centreText(this.game.add.bitmapText(0,175, 'green_font', Mainframe.corpName[0] +' Data Obtained: ', 36), this.game.width);
-		Mainframe.centreSprite(this.game.add.sprite(0, 200, 'atlas', 'Victory_Screen/data_obtained_bar.png'), this.game.width);
+		this.elementLayer.add(Mainframe.centreSprite(this.game.add.sprite(0, 70, 'atlas', 'Victory_Screen/run_successful.png'), this.game.width));
+		this.elementLayer.add(Mainframe.centreText(this.game.add.bitmapText(0,175, 'green_font', Mainframe.corpName[0] +' Data Obtained: ', 36), this.game.width));
+		this.elementLayer.add(Mainframe.centreSprite(this.game.add.sprite(0, 200, 'atlas', 'Victory_Screen/data_obtained_bar.png'), this.game.width));
 
-		this.game.add.bitmapText(276,260, 'green_font', 'Corp Difficulty: ', 28);
-		this.game.add.bitmapText(333,285, 'green_font', 'Data Value: ', 28);
-		this.game.add.bitmapText(304,310, 'green_font', 'Total Earned: ', 28);
+		this.elementLayer.add(this.game.add.bitmapText(276,260, 'green_font', 'Corp Difficulty: ', 28));
+		this.elementLayer.add(this.game.add.bitmapText(333,285, 'green_font', 'Data Value: ', 28));
+		this.elementLayer.add(this.game.add.bitmapText(304,310, 'green_font', 'Total Earned: ', 28));
 
-		this.game.add.bitmapText(480, 260, 'green_font', Mainframe.corpDifficulty.toFixed(1) + 'x', 28);
+		this.elementLayer.add(this.game.add.bitmapText(480, 260, 'green_font', Mainframe.corpDifficulty.toFixed(1) + 'x', 28));
 
 		var percentage = 100 - ((3 - Mainframe.hackerProxies.length ) * 20)
 		var value = percentage * 48;
@@ -34,9 +39,12 @@ Mainframe.VictoryScreen.prototype = {
 		Mainframe.totalEarned += value;
 
 		this.dataValueText = this.game.add.bitmapText(480, 285, 'green_font', value + 'C', 28);
+		this.elementLayer.add(this.dataValueText);
 		this.totalEarnedText = this.game.add.bitmapText(480, 310, 'green_font', Mainframe.totalEarned + 'C', 28);
+		this.elementLayer.add(this.totalEarnedText);
 
 		var b = this.game.add.sprite(121, 217,'atlas', 'Subroutines/General/trace_bar_full.png');
+		this.elementLayer.add(b)
 		b.width = 0;
 		b.height = 30;
 		b.alpha = 0.8;
@@ -45,11 +53,14 @@ Mainframe.VictoryScreen.prototype = {
 
 		this.nextCorpOption = this.game.add.bitmapText(0,400, 'green_font', 'Next Corp', 30);
 		Mainframe.centreText(this.nextCorpOption, this.game.width);
+		this.elementLayer.add(this.nextCorpOption);
 
 		this.exitOption = this.game.add.bitmapText(0,430, 'green_font', 'Exit', 30);
 		Mainframe.centreText(this.exitOption, this.game.width);
+		this.elementLayer.add(this.exitOption);
 
 		this.selectionIcon = this.game.add.bitmapText(200,430, 'green_font', '>', 30);
+		this.elementLayer.add(this.selectionIcon);
 
 		var cursors = this.game.input.keyboard.createCursorKeys();
 
@@ -63,10 +74,11 @@ Mainframe.VictoryScreen.prototype = {
 
 		this.repositionSelector();
 		var bg_flicker = this.game.add.sprite(0, 0, 'bg_flicker');
+		this.elementLayer.add(bg_flicker);
 		bg_flicker.animations.add('anim');
 		bg_flicker.animations.play('anim', 16, false);
 
-		this.game.add.sprite(0,0,'atlas','General/monitor.png');
+		this.monitorLayer.add(this.game.add.sprite(0,0,'atlas','General/monitor.png'))
 	},
 
 	toggleSelection: function () {
@@ -86,6 +98,7 @@ Mainframe.VictoryScreen.prototype = {
 
 	select: function () {
 		var bg_flicker = this.game.add.sprite(0, 0, 'bg_flicker_on');
+		this.elementLayer.add(bg_flicker);
 		bg_flicker.animations.add('anim');
 		bg_flicker.animations.play('anim', 16, false);
 		bg_flicker.events.onAnimationComplete.add(function () {
